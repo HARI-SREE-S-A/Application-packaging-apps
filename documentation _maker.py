48d3741cd5f760384cbdf29959024ea9f0824aba
HARISREE SURESH <getharisree@outlook.com>
2023-03-21 14:01:41 +0530

    Update documentation _maker.py

--- documentation _maker.py
+++ documentation _maker.py
@@ -1,1 +1,21 @@
+import openai
 
+input_text = str(input("Enter the query"))
+
+openai.api_key = "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"
+
+model_engine = "text-davinci-003"
+prompt = input_text
+
+# Generate a response
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+response = completion.choices[0].text
+print(response)
